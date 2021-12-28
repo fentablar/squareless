c7ce4aa9688446589d4afb907167bdb1b30f5ab6
fentablar <fentablar@gmail.com>
2021-12-28 06:30:01 -0800

    add scroll container testing

---
+++ js/scrollContainerTesting.js
@@ -0,0 +1,25 @@
+const main = document.querySelector('main');
+
+main.addEventListener('click', function () {
+  this.classList.toggle('pivot');
+});
+
+const buildCard = () => {
+  if (!cardWrap) return;
+
+  let rnd = Math.floor(Math.random() * 10) + 3;
+
+  for (let i = 0; i < rnd; i++) {
+    let cardFrame = document.createElement('div');
+    cardFrame.classList.add('cardFrame');
+    cardWrap.append(cardFrame);
+
+    let card = document.createElement('div');
+    let crdTxt = '<p>Card</p><p>' + i + '</p>';
+    card.classList.add('card');
+    card.insertAdjacentHTML('afterbegin', crdTxt);
+    cardFrame.append(card);
+  }
+}
+
+buildCard();
