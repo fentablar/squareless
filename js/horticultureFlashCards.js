049758e442fcff65a9e7e2420fcc28db8778e50b
fentablar <fentablar@gmail.com>
2022-01-07 22:40:02 -0800

    make buttons thicker

--- js/horticultureFlashCards.js
+++ js/horticultureFlashCards.js
@@ -141,7 +141,7 @@ const controls = {
     const innerWrap = addElement(outerWrap, 'div', 'innerWrap');
 
     const prvIco = '<svg xmlns="http://www.w3.org/2000/svg" '
-      + 'width="24" height="24" viewBox="0 0 24 24" stroke-width="2" '
+      + 'width="24" height="24" viewBox="0 0 24 24" stroke-width="3" '
       + 'stroke="currentColor" fill="none" stroke-linecap="round" '
       + 'stroke-linejoin="round">'
       + '<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>'
@@ -149,7 +149,7 @@ const controls = {
     const prev = addWrappedButton(innerWrap, 'button', prvIco, 'iterate', 'prev');
 
     const nxtIco = '<svg xmlns="http://www.w3.org/2000/svg" '
-      + 'width="24" height="24" viewBox="0 0 24 24" stroke-width="2" '
+      + 'width="24" height="24" viewBox="0 0 24 24" stroke-width="3" '
       + 'stroke="currentColor" fill="none" stroke-linecap="round" '
       + 'stroke-linejoin="round">'
       + '<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>'
@@ -157,7 +157,7 @@ const controls = {
     const next = addWrappedButton(innerWrap, 'button', nxtIco, 'iterate', 'next');
 
     const rstIco = '<svg xmlns="http://www.w3.org/2000/svg" '
-      + 'width="24" height="24" viewBox="0 0 24 24" stroke-width="2" '
+      + 'width="24" height="24" viewBox="0 0 24 24" stroke-width="3" '
       + 'stroke="currentColor" fill="none" stroke-linecap="round" '
       + 'stroke-linejoin="round">'
       + '<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>'
